95595397774c6dd817b785a095b273209ccae694
dingyue <dingyue0724@gmail.com>
2017-12-18 21:43:36 +0800

    蘑菇街3d动画及轮播图的完成上传

--- meilis/fc/index.js
+++ meilis/fc/index.js
@@ -1,6 +1,42 @@
 $(function () {
+	/* 记录导航栏的轮播图的背景颜色 */
+	var arrColor = ['#115b44', '#fc2843', '#ff6819', '#ffb731', '#dd362e', '#ff989e'];
+
+	var timer = null;		// 设置导航栏定时器
+	var timerList = null;	// 设置导航栏列表的定时器
+	var timer1 = null;		// 设置导航栏轮播图下标定时器
+	var timer2 = null;		// 设置导航栏轮播图图片定时器
+	var timer3 = null;		// 设置3d图旋转的定时器
+	var timer4 = null;		// 设置快抢发新日轮播图的定时器
+	var timer5 = null;		// 设置精选专题的轮播图的定时器
+	var tM = null;			// 固定导航栏的下拉菜单
+	var tF = null;
+	var count = null;		// 定义定时器
+	var num = 0;			// 顶部导航栏轮播图的下标计数
+	let mm = 0;				// 精选专题的轮播图的下标计数
+	
+	/* 商品倒计时 */
+	var actime = [];		
+	actime = $('.countdown>p').html().split(':');	// 获取设定的时间
+	var allTime = Number(actime[0]) * 3600 + Number(actime[1]) * 60 + Number(actime[2]);	// 计算倒计时的总时间 (s)
+	
+	/* 红人穿搭图片的3d旋转 */
+	var rotateNum = 0;		// 3d旋转图片的计数变量
+	var f = true;			// 定义3d图片旋转循环一遍完成的标志位
+	var $imgsLis = $('.special > .main-list > .img-ad-list > .imgs > ul > li.l-imgs');
+
+	
+	/* 导航栏轮播图处理 */
+	var $ulis = $('.slide-img>ul>li');
+
+	/* 品牌特卖及以下的图片处理 */
+	let $imgAnimt = $('.special>.img-animt>div:nth-child(2)');
+	let $imgsAnimt = $('.special>.img-animt>.imgs-animt');
+
+	/* 浏览器的窗口的高度 */
+	var Height = $(window).height();	
+	
 	/* 导航栏 */
-	var timer = null;
 	$('.navTop>ul>li:eq(2), .shop-cart').mouseenter(function (ev) {
 		clearTimeout(timer);
 		$('.shop-cart').stop().show().siblings('div').stop().hide();
@@ -39,7 +75,6 @@ $(function () {
 		$('.select-box>span').html('搜' + $(this).children().html());
 	});
 
-	var timerList = null;
 	/* 导航栏列表 */
 	for (let i = 0; i < $('.market>ul>li').length; i++) {
 		$('.market>ul>li').eq(i).hover(function () {
@@ -60,7 +95,6 @@ $(function () {
 		});
 	}
 
-
 	/* 轮播图 */
 	/* 轮播图下标 */
 	for (var i = 0; i < $('.slide-img>ul>li').length; i++) {
@@ -69,15 +103,8 @@ $(function () {
 	}
 	$('.slide-img>ol>li').eq(0).attr("class", "active");
 
-	/* 记录背景颜色 */
-	var arrColor = ['#f07', '#f06ae9', '#7e7aff', '#ffcad2', '#62b1db', '#177bb1'];
-
-	var timer = null;
-	var timer2 = null;
-	var $ulis = $('.slide-img>ul>li');
-	var num = 0;
 	$('.slide-cont').css('background', arrColor[num]);
-	timer = setInterval(function () {
+	timer1 = setInterval(function () {
 		if (num > $ulis.length - 1) {
 			num = 0;
 		}
@@ -93,7 +120,7 @@ $(function () {
 	for (let i = 0; i < $('.slide-img>ol>li').length; i++) {
 		$('.slide-img>ol>li').eq(i).hover(function () {
 			// console.log('图片下标：' + i);
-			clearInterval(timer);
+			clearInterval(timer1);
 			clearInterval(timer2);
 			$(this).attr('class', 'active').siblings().attr('class', '');
 			$('.slide-img>ul>li').eq(i).animate({ "opacity": 1 }, 1000, "linear").siblings().animate({ "opacity": 0 });
@@ -117,7 +144,7 @@ $(function () {
 		/* 悬停暂停图片 */
 		$('.slide-img>ul>li').eq(j).hover(function () {
 			// console.log('现在暂停在：' + j);
-			clearInterval(timer);
+			clearInterval(timer1);
 			clearInterval(timer2);
 		}, function () {
 			// console.log('鼠标离开了');
@@ -136,7 +163,7 @@ $(function () {
 		});
 	}
 
-	/* 右侧导航啦 */
+	/* 右侧导航 */
 	for (let i = 0; i < $('.r-sidebar>ol>li').length - 1; i++) {
 		$('.r-sidebar>ol>li').eq(i).hover(function () {
 			$(this).css('background', '#ef2f23').prev('li').children('a').css('border-bottom', '1px solid #202020');
@@ -159,19 +186,28 @@ $(function () {
 		$(this).children('img').stop().animate({ "right": '20px' }, 200, 'linear');
 	});
 
-	/* 红人穿搭 */
+	/* 快抢发新日 -- 轮播图部分 */
+	function scroll() {
+		$(".new-cont > div:last-child > ul").stop().animate({ "margin-left": "-1000px" }, 800, 'linear', function () {
+			for (var i = 0; i < 5; i++) {
+				$(".new-cont > div:last-child > ul > li:eq(0)").appendTo($(".new-cont > div:last-child > ul"));
+			}
+			$(".new-cont > div:last-child > ul").css({ "margin-left": 0 })
+		})
+	}
+	timer4 = setInterval(scroll, 5000);
+	// 鼠标移入移出
+	$(".new-cont > div:last-child > ul").hover(function () {
+		clearInterval(timer4);
+		$('.new-cont > div:last-child > span').show();
+	}, function () {
+		timer4 = setInterval(scroll, 5000);
+		$('.new-cont > div:last-child > span').hide();
+	});
 
-	/* 快抢发新日 */
-	/* 轮播图部分 */
-	// var timerNew = null;
-	// timerNew = setInterval(function () {
-
-	// }, 2000);
+	// 搜索框的api http://list.mogujie.com/s?q=围巾&ptp=1.eW5XD.0.0.LNAf6jV
 
 	/* 品牌特卖及以下的图片处理 */
-	let $imgAnimt = $('.special>.img-animt>div:nth-child(2)');
-	let $imgsAnimt = $('.special>.img-animt>.imgs-animt');
-
 	$('.special>.img-animt>div:nth-child(2)>.ad-txt, .special>.img-animt>div:nth-child(2)>a').hover(function () {
 		$(this).parent().children('a').children('img').stop().animate({ 'top': '-5px' }, 200, 'linear');
 	}, function () {
@@ -184,7 +220,6 @@ $(function () {
 	});
 
 	/* 固定导航栏 & 回到顶部 */
-	var Height = $(window).height();
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 0) {
 			$('.r-sidebar>ol>li:last-child').css('display', 'block');
@@ -198,8 +233,8 @@ $(function () {
 		}
 		leader = $(this).scrollTop();
 	});
+
 	/* 固定导航栏的下拉菜单 */
-	var tM = null;
 	$('.fixed-nav>.nav-t1').hover(function () {
 		$('.fixed-nav>ul').css('display', 'block');
 		$('.fixed-nav>ol').css('display', 'block');
@@ -209,7 +244,6 @@ $(function () {
 			$('.fixed-nav>ol').css('display', 'none');
 		}, 200);
 	});
-	var tF = null;
 	for (let i = 0; i < $('.fixed-nav>ul>li').length; i++) {
 		$('.fixed-nav>ul>li').eq(i).hover(function () {
 			clearInterval(tF);
@@ -236,19 +270,11 @@ $(function () {
 		});
 	}
 	/* 回到顶部 */
-	var back = null;
 	$('.r-sidebar>ol>li:last-child').click(function () {
 		$(window).scrollTop(0);
 	});
 
 	/* 商品倒计时 */
-	var count = null;	// 定义定时器
-	var actime = [];
-	actime = $('.countdown>p').html().split(':');
-
-	// 计算总时间 (s)
-	var allTime = Number(actime[0]) * 3600 + Number(actime[1]) * 60 + Number(actime[2]);
-	// 倒计时函数
 	function countDown() {
 		count = setInterval(function () {
 			allTime--;
@@ -273,20 +299,11 @@ $(function () {
 	}
 	countDown();
 
-	// 红人穿搭图片的3d旋转
-	var rotateNum = 0;
-	var f = true;
-	var v = 1;
-	var timer3 = null;
-	var $imgsLis = $('.special > .main-list > .img-ad-list > .imgs > ul > li.l-imgs');
-
-	console.log($imgsLis.length-1);
-
+	/* 红人穿搭图片的3d旋转 */
 	timer3 = setInterval(function () {
 		if (rotateNum == $imgsLis.length) {
 			rotateNum = 0;
 			f = !f;
-			v++;
 		}
 		if (f) {
 			$imgsLis.eq(rotateNum).children('div.inside-img').css({
@@ -297,8 +314,45 @@ $(function () {
 				'WebkitTransform': 'rotateY(0)',
 			});
 		}
-		console.log(rotateNum);
 		rotateNum++;
 	}, 5000);
+
+	/* 精选专题 轮播图 */
+	function scroll2() {
+		if (mm >= ($(".careful > .list-f > ul > li").length / 3 - 1)) {
+			mm = -1;
+		}
+		$(".careful > .list-f > ul").stop().animate({ "margin-left": "-950px" }, 500, 'linear', function () {
+			for (var i = 0; i < 3; i++) {
+				$(".careful > .list-f > ul > li:eq(0)").appendTo($(".careful > .list-f > ul"));
+			}
+			$(".careful > .list-f > ul").css({ "margin-left": 0 })
+		})
+		mm++;
+		$('.list-title > .upList > li').eq(mm).attr('class', 'focusCurrent').siblings().attr('class', '');
+	}
+	timer5 = setInterval(scroll2(mm), 5000);
+
+	// 鼠标移入移出
+	$(".careful > .list-f > ul").hover(function () {
+		clearInterval(timer5);
+		$('.careful > .list-f > span.all').stop().show();
+	}, function () {
+		timer5 = setInterval(scroll2, 5000);
+		$('.careful > .list-f > span.all').stop().hide();
+	});
+
+	for (let i = 0; i < $('.list-title > .upList > li').length; i++) {
+		$('.list-title > .upList > li').eq(i).click(function () {
+			clearInterval(timer5);
+			$(this).attr('class', 'focusCurrent').siblings().attr('class', '');
+			// console.log($(this).index());
+			// mm = $(this).index() - 1;
+			// scroll2();
+		});
+	}
+	$('.list-title > .upList > li').mouseleave(function () {
+		timer5 = setInterval(scroll2, 5000);
+	});
 
 });
