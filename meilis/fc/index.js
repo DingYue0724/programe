b64cb9e2f02c3b558606c72226c4e803e634fd78
dingyue <dingyue0724@gmail.com>
2017-12-21 09:39:11 +0800

    轮播图的完善

--- meilis/fc/index.js
+++ meilis/fc/index.js
@@ -14,27 +14,49 @@ $(function () {
 	var count = null;		// 定义定时器
 	var num = 0;			// 顶部导航栏轮播图的下标计数
 	let mm = 0;				// 精选专题的轮播图的下标计数
+
+	/* 快抢发新日 */
+	var new_list_num = 0;	// 图片
+	var $new_cont_list = $(".new-cont > div:last-child > div.new-cont-list");
+	var $new_cont_div_last_scroll = $(".new-cont > div:last-child > div.new-cont-list > div.new-cont-scroll");
+	
+	/* 导航栏轮播图处理 */
+	var $ulis = $('.slide-img>ul>li');
 	
 	/* 商品倒计时 */
-	var actime = [];		
+	var actime = [];
 	actime = $('.countdown>p').html().split(':');	// 获取设定的时间
 	var allTime = Number(actime[0]) * 3600 + Number(actime[1]) * 60 + Number(actime[2]);	// 计算倒计时的总时间 (s)
-	
+
 	/* 红人穿搭图片的3d旋转 */
 	var rotateNum = 0;		// 3d旋转图片的计数变量
 	var f = true;			// 定义3d图片旋转循环一遍完成的标志位
 	var $imgsLis = $('.special > .main-list > .img-ad-list > .imgs > ul > li.l-imgs');
-
 	
-	/* 导航栏轮播图处理 */
-	var $ulis = $('.slide-img>ul>li');
-
 	/* 品牌特卖及以下的图片处理 */
 	let $imgAnimt = $('.special>.img-animt>div:nth-child(2)');
 	let $imgsAnimt = $('.special>.img-animt>.imgs-animt');
 
 	/* 浏览器的窗口的高度 */
-	var Height = $(window).height();	
+	var Height = $(window).height();
+
+	/* 精品推荐轮播图 */
+	var list_num = 0;		// 图片
+	var list_up_num = 0;	// 下标
+	var $list_f_cont = $('.careful > .list-f > .list-f-cont');
+	var $list_f_scroll = $('.careful> .list-f > .list-f-cont  > div.list-f-scroll');
+	var $upList_li = $('.list-title > .upList > li');
+	var $list_f_span_all = $('.careful > .list-f > span.all');
+
+	/* 导航栏列表 */
+	var $market_ul_li = $('.market>ul>li');
+	var $market_ol_li = $('.market>ol>li');
+
+	/* 固定导航栏 */
+	var $fixed_nav_ul = $('.fixed-nav>ul');
+	var $fixed_nav_ol = $('.fixed-nav>ol');
+
+	/* --------------------------------------------------------------------------------- */
 	
 	/* 导航栏 */
 	$('.navTop>ul>li:eq(2), .shop-cart').mouseenter(function (ev) {
@@ -76,21 +98,21 @@ $(function () {
 	});
 
 	/* 导航栏列表 */
-	for (let i = 0; i < $('.market>ul>li').length; i++) {
-		$('.market>ul>li').eq(i).hover(function () {
+	for (let i = 0; i < $market_ul_li.length; i++) {
+		$market_ul_li.eq(i).hover(function () {
 			clearInterval(timerList);
-			$('.market>ol>li').eq(i).css('display', 'block').siblings('li').css('display', 'none');
+			$market_ol_li.eq(i).css('display', 'block').siblings('li').css('display', 'none');
 		}, function () {
 			timerList = setInterval(function () {
-				$('.market>ol>li').eq(i).css('display', 'none');
+				$market_ol_li.eq(i).css('display', 'none');
 			}, 200);
 		});
-		$('.market>ol>li').eq(i).hover(function () {
+		$market_ol_li.eq(i).hover(function () {
 			clearInterval(timerList);
 			$(this).eq(i).css('display', 'block').siblings('li').css('display', 'none');
 		}, function () {
 			timerList = setInterval(function () {
-				$('.market>ol>li').eq(i).css('display', 'none');
+				$market_ol_li.eq(i).css('display', 'none');
 			}, 200);
 		});
 	}
@@ -104,64 +126,39 @@ $(function () {
 	$('.slide-img>ol>li').eq(0).attr("class", "active");
 
 	$('.slide-cont').css('background', arrColor[num]);
-	timer1 = setInterval(function () {
+	function navImgscroll () {
 		if (num > $ulis.length - 1) {
 			num = 0;
 		}
-		num++;
-		// console.log('定时器计时显示图片：' + num);
+		
 		$('.slide-img>ol').css('zIndex', '200');
 		$ulis.eq(num).animate({ "opacity": 1 }, 2000, "linear").siblings().animate({ "opacity": 0 });
 		$('.slide-img>ol>li').eq(num).attr("class", "active").siblings().attr('class', '');
 		$('.slide-cont').css('background', arrColor[num]);
 		$ulis.eq(num).css('zIndex', '100').siblings().css('zIndex', '90');
-	}, 5000);
+		num++;
+	}
+	
 	/* 悬停下标显示图片 */
 	for (let i = 0; i < $('.slide-img>ol>li').length; i++) {
 		$('.slide-img>ol>li').eq(i).hover(function () {
-			// console.log('图片下标：' + i);
 			clearInterval(timer1);
-			clearInterval(timer2);
 			$(this).attr('class', 'active').siblings().attr('class', '');
 			$('.slide-img>ul>li').eq(i).animate({ "opacity": 1 }, 1000, "linear").siblings().animate({ "opacity": 0 });
 			$('.slide-cont').css('background', arrColor[i]);
 		}, function () {
-			timer2 = setInterval(function () {
-				if (i > $ulis.length - 1) {
-					i = 0;
-				}
-				i++;
-				// console.log('鼠标离开下标：' + i);
-				$('.slide-img>ol').css('zIndex', '200');
-				$ulis.eq(i).animate({ "opacity": 1 }, 2000, "linear").siblings().animate({ "opacity": 0 });
-				$('.slide-img>ol>li').eq(i).attr("class", "active").siblings().attr('class', '');
-				$('.slide-cont').css('background', arrColor[i]);
-				$ulis.eq(i).css('zIndex', '100').siblings().css('zIndex', '90');
-			}, 5000);
+			num = i;
 		});
 	}
 	for (var j = 0; j < $('.slide-img>ol>li').length; j++) {
 		/* 悬停暂停图片 */
 		$('.slide-img>ul>li').eq(j).hover(function () {
-			// console.log('现在暂停在：' + j);
 			clearInterval(timer1);
-			clearInterval(timer2);
 		}, function () {
-			// console.log('鼠标离开了');
-			timer2 = setInterval(function () {
-				if (j > $ulis.length - 1) {
-					j = 0;
-				}
-				j++;
-				// console.log('现在显示的图片是：' + j);
-				$('.slide-img>ol').css('zIndex', '200');
-				$ulis.eq(j).animate({ "opacity": 1 }, 2000, "linear").siblings().animate({ "opacity": 0 });
-				$('.slide-img>ol>li').eq(j).attr("class", "active").siblings().attr('class', '');
-				$('.slide-cont').css('background', arrColor[j]);
-				$ulis.eq(j).css('zIndex', '100').siblings().css('zIndex', '90');
-			}, 5000);
+			timer1 = setInterval(navImgscroll, 2000);
 		});
 	}
+	timer1 = setInterval(navImgscroll, 2000);
 
 	/* 右侧导航 */
 	for (let i = 0; i < $('.r-sidebar>ol>li').length - 1; i++) {
@@ -184,25 +181,6 @@ $(function () {
 		$(this).children('img').stop().animate({ "right": '25px' }, 200, 'linear');
 	}, function () {
 		$(this).children('img').stop().animate({ "right": '20px' }, 200, 'linear');
-	});
-
-	/* 快抢发新日 -- 轮播图部分 */
-	function scroll() {
-		$(".new-cont > div:last-child > ul").stop().animate({ "margin-left": "-1000px" }, 800, 'linear', function () {
-			for (var i = 0; i < 5; i++) {
-				$(".new-cont > div:last-child > ul > li:eq(0)").appendTo($(".new-cont > div:last-child > ul"));
-			}
-			$(".new-cont > div:last-child > ul").css({ "margin-left": 0 })
-		})
-	}
-	timer4 = setInterval(scroll, 5000);
-	// 鼠标移入移出
-	$(".new-cont > div:last-child > ul").hover(function () {
-		clearInterval(timer4);
-		$('.new-cont > div:last-child > span').show();
-	}, function () {
-		timer4 = setInterval(scroll, 5000);
-		$('.new-cont > div:last-child > span').hide();
 	});
 
 	// 搜索框的api http://list.mogujie.com/s?q=围巾&ptp=1.eW5XD.0.0.LNAf6jV
@@ -236,36 +214,36 @@ $(function () {
 
 	/* 固定导航栏的下拉菜单 */
 	$('.fixed-nav>.nav-t1').hover(function () {
-		$('.fixed-nav>ul').css('display', 'block');
-		$('.fixed-nav>ol').css('display', 'block');
+		$fixed_nav_ul.css('display', 'block');
+		$fixed_nav_ol.css('display', 'block');
 	}, function () {
 		tM = setTimeout(function () {
-			$('.fixed-nav>ul').css('display', 'none');
-			$('.fixed-nav>ol').css('display', 'none');
+			$fixed_nav_ul.css('display', 'none');
+			$fixed_nav_ol.css('display', 'none');
 		}, 200);
 	});
 	for (let i = 0; i < $('.fixed-nav>ul>li').length; i++) {
-		$('.fixed-nav>ul>li').eq(i).hover(function () {
+		$fixed_nav_ul.children('li').eq(i).hover(function () {
 			clearInterval(tF);
 			clearInterval(tM);
-			$('.fixed-nav>ul').css('display', 'block');
-			$('.fixed-nav>ol>li').eq(i).css('display', 'block').siblings('li').css('display', 'none');
+			$fixed_nav_ul.css('display', 'block');
+			$fixed_nav_ol.children('li').eq(i).css('display', 'block').siblings('li').css('display', 'none');
 		}, function () {
 			clearInterval(tM);
 			tF = setTimeout(function () {
-				$('.fixed-nav>ol>li').eq(i).css('display', 'none');
-				$('.fixed-nav>ul').css('display', 'none');
+				$fixed_nav_ol.children('li').eq(i).css('display', 'none');
+				$fixed_nav_ul.css('display', 'none');
 			}, 200);
 		});
-		$('.fixed-nav>ol>li').eq(i).hover(function () {
+		$fixed_nav_ol.children('li').eq(i).hover(function () {
 			clearInterval(tF);
 			clearInterval(tM);
 			$(this).eq(i).css('display', 'block').siblings('li').css('display', 'none');
 		}, function () {
 			clearInterval(tM);
 			tF = setTimeout(function () {
-				$('.fixed-nav>ol').css('display', 'none');
-				$('.fixed-nav>ul').css('display', 'none');
+				$fixed_nav_ol.css('display', 'none');
+				$fixed_nav_ul.css('display', 'none');
 			}, 200);
 		});
 	}
@@ -317,42 +295,95 @@ $(function () {
 		rotateNum++;
 	}, 5000);
 
-	/* 精选专题 轮播图 */
-	function scroll2() {
-		if (mm >= ($(".careful > .list-f > ul > li").length / 3 - 1)) {
-			mm = -1;
+	/* 快抢发新日 -- 轮播图部分 */
+	$new_cont_div_last_scroll.eq(0).clone(true).appendTo($new_cont_list);
+	function scroll() {
+		new_list_num++;		// 图片
+		if (new_list_num > $new_cont_div_last_scroll.length) {
+			new_list_num = 1;
+			$new_cont_list.css('left', '0');
 		}
-		$(".careful > .list-f > ul").stop().animate({ "margin-left": "-950px" }, 500, 'linear', function () {
-			for (var i = 0; i < 3; i++) {
-				$(".careful > .list-f > ul > li:eq(0)").appendTo($(".careful > .list-f > ul"));
-			}
-			$(".careful > .list-f > ul").css({ "margin-left": 0 })
-		})
-		mm++;
-		$('.list-title > .upList > li').eq(mm).attr('class', 'focusCurrent').siblings().attr('class', '');
+		$new_cont_list.stop().animate({ 'left': -new_list_num * 1000 }, 800, 'linear');
 	}
-	timer5 = setInterval(scroll2(mm), 5000);
+	timer4 = setInterval(scroll, 3000);
+	// 鼠标移入移出
+	$('.new-cont > div:last-child > span, .new-cont > div:last-child > div.new-cont-list').hover(function () {
+		clearInterval(timer4);
+		$new_cont_list.siblings('span').show();
+	}, function () {
+		timer4 = setInterval(scroll, 3000);
+		$new_cont_list.siblings('span').hide();
+	});
+	// 左右点击滑动
+	$(".new-cont > div:last-child > span.lf").click(function () {
+		new_list_num--;
+		if (new_list_num < 0) {
+			new_list_num = $new_cont_div_last_scroll.length - 1;
+			$new_cont_list.css('left', -$new_cont_div_last_scroll.length * 1000 + 'px');
+		}
+		$new_cont_list.stop().animate({ 'left': -new_list_num * 1000 }, 800, 'linear');
+	});
+	$(".new-cont > div:last-child > span.rght").click(function () {
+		scroll();
+	});
+
+	/* 精选专题 轮播图 */
+	$list_f_scroll.eq(0).clone(true).appendTo($list_f_cont);
+	function scroll2() {
+		list_num++;		// 图片
+		list_up_num++;	// 下标
+		if (list_num > $list_f_scroll.length) {
+			list_num = 1;
+			$list_f_cont.css('left', '0');
+		}
+		$list_f_cont.stop().animate({ 'left': -list_num * 950 }, 800, 'linear');
+
+		if (list_up_num > $upList_li.length - 1) {
+			list_up_num = 0;
+		}
+		$upList_li.eq(list_up_num).attr('class', 'focusCurrent').siblings().attr('class', '');
+	}
+	timer5 = setInterval(scroll2, 4000);
 
 	// 鼠标移入移出
-	$(".careful > .list-f > ul").hover(function () {
+	$(".careful > .list-f > .list-f-cont > div.list-f-scroll, .careful > .list-f > span.all").hover(function () {
 		clearInterval(timer5);
-		$('.careful > .list-f > span.all').stop().show();
+		$list_f_span_all.stop().show();
 	}, function () {
-		timer5 = setInterval(scroll2, 5000);
-		$('.careful > .list-f > span.all').stop().hide();
+		timer5 = setInterval(scroll2, 4000);
+		$list_f_span_all.stop().hide();
 	});
 
-	for (let i = 0; i < $('.list-title > .upList > li').length; i++) {
-		$('.list-title > .upList > li').eq(i).click(function () {
+	for (let i = 0; i < $upList_li.length; i++) {
+		$upList_li.eq(i).hover(function () {
 			clearInterval(timer5);
 			$(this).attr('class', 'focusCurrent').siblings().attr('class', '');
-			// console.log($(this).index());
-			// mm = $(this).index() - 1;
-			// scroll2();
+			$list_f_cont.stop().animate({ 'left': -i * 950 }, 1000, 'linear');
+		}, function () {
+			list_num = i;
+			list_up_num = i;
 		});
 	}
-	$('.list-title > .upList > li').mouseleave(function () {
-		timer5 = setInterval(scroll2, 5000);
+	$upList_li.mouseleave(function () {
+		timer5 = setInterval(scroll2, 4000);
 	});
 
+	$('.careful > .list-f > span.r').click(function () {
+		scroll2();
+	});
+	$('.careful > .list-f > span.l').click(function () {
+		list_num--;
+		list_up_num--;
+		if (list_num < 0) {
+			list_num = $list_f_scroll.length - 1;
+			$list_f_cont.css('left', -$list_f_scroll.length * 950 + 'px');
+		}
+		if (list_up_num < 0) {
+			list_up_num = $upList_li.length - 1;
+		}
+		console.log(list_num);
+		console.log(list_up_num);
+		$list_f_cont.stop().animate({ 'left': -list_num * 950 }, 800, 'linear');
+		$upList_li.eq(list_up_num).attr('class', 'focusCurrent').siblings().attr('class', '');
+	});
 });
